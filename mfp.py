dc6bfbba8dc8be2d0892eff6b85eaf6e56595abe
Ostap Marysiuk <ostapmarostap@gmail.com>
2018-06-19 20:08:33 +0300

    Have made corrections according to @al-serebrov advice

--- mfp.py
+++ mfp.py
@@ -1,30 +1,37 @@
-print('\nHi dude!')
-print('Whats your name?\n')
+# Messages block
 
+hi_message = '''
+Hi dude!
+Whats your name?
+'''
+
+print(hi_message)
 usr_name = input()
-print(' ')
 
-print('So, ' + usr_name + ' is my homie.')
-print('How old are you?\n')
+age_message = '''
+Nice to meet you!
+How old are you {}?
+'''.format(usr_name)
+
+prognosis_message = '''
+Holy cow, {0}, you have {1} years left!
+If I had {1} years I\'ll learn programming everyday!
+'''
+false_message = '''
+No way {}! You can\'t be so old! Type your real age.
+'''
+
+
+print(age_message)
 
 while True:
     usr_age = input()
-    print(' ')
     if int(usr_age) < 100:
 
         years_left = str(100 - int(usr_age))
-
-        print(
-            'Holy cow, {0}, you have {1} years left! \nIf I had {1} years I\'ll learn \
-programming everyday!\n'.format(
-                usr_name,
-                years_left
-            )
-        )
+        print(prognosis_message.format(usr_name, years_left))
         break
     else:
-        print('No way {}! You can\'t be so old! Type your real age.\n'.format(
-            usr_name
-        ))
+        print(false_message.format(usr_name))
 
 print('Good Luck, {}!\n'.format(usr_name))
